d41fad9651c9e830c5baa6316fe4770c316b409e
Simon Timms <stimms@gmail.com>
2019-08-28 22:16:08 -0600

    Initial add

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using CsvHelper;
+using System;
+using System.Linq;
+using System.IO;
+using System.Collections.Generic;
+
+namespace LargeDataLoop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+using (var reader = new StreamReader(@"HBCDBiosolids_EN_FR.csv"))
+using (var csv = new CsvReader(reader))
+{
+    var records = csv.GetRecords<Record>().ToList();
+    var mean = records.Sum(x => x.CONCENTRATION_FOUND) / records.Count();
+    var standardDeviation = Math.Sqrt(records.Sum(x => Math.Pow(x.CONCENTRATION_FOUND - mean, 2) / records.Count()));
+    var cleanRecords = new List<Record>();
+    foreach (var record in records)
+    {
+        var zScore = Convert.ToDouble(record.CONCENTRATION_FOUND - mean) / standardDeviation;
+        if (Math.Abs(zScore) > 2)
+            Console.WriteLine($"{record.CONCENTRATION_FOUND} - {zScore}");
+        else
+            cleanRecords.Add(record);
+    }
+}
+        }
+    }
+}
---
+++ Record.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LargeDataLoop
+{
+    class Record
+    {
+        public string SAMPLE_NAME { get; set; }
+        public string WWTP_CODE { get; set; }
+        public string LOCATION { get; set; }
+        public int NUMBER { get; set; }
+        public string AXYS_ID { get; set; }
+        public string COMPOUND { get; set; }
+        public string LAB_FLAG { get; set; }
+        public double CONCENTRATION_FOUND { get; set; }
+        public string DETECTION_LIMIT { get; set; }
+        public string UNIT { get; set; }
+        public DateTime COLLECTION_DATE { get; set; }
+        public string WORKGROUP { get; set; }
+
+    }
+}
